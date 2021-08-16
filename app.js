c846eb2b4dba70ced2dac29fc757bf6a801db616
Steven Zebrowski <steve@omegasupreme.local>
2021-08-16 12:46:07 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Greetings Globe!'));
+app.get('/', (req, res) => res.send('Salutations Sphere!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
